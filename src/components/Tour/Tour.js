fe5999410a695d0040a5ca5a46b4258b3cc02d65
Peter <Posoo617@gmail.com>
2020-07-26 15:40:17 +0300

    Add different cities using map, details

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -3,30 +3,25 @@ import "./Tour.scss"
 
 class Tour extends Component {
 	render() {
+		const { city, img, name, info } = this.props.tour
 		return (
 			<article className="tour">
 				<div className="img-container">
-					<img
-						src="https://images.pexels.com/photos/2422461/pexels-photo-2422461.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-						alt=""
-					/>
+					<img src={img} alt="City Image" />
 					<span className="close-btn">
 						<i className="fas fa-window-close"></i>
 					</span>
 				</div>
 				<div className="tour-info">
-					<h3>city</h3>
-					<h4>name</h4>
+					<h3>{city}</h3>
+					<h4>{name}</h4>
 					<h5>
 						info{" "}
 						<span>
 							<i className="fas fa-caret-square-down"></i>{" "}
 						</span>{" "}
 					</h5>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Accusantium, assumenda?
-					</p>
+					<p>{info}</p>
 				</div>
 			</article>
 		)
--- src/components/TourList/TourList.js
+++ src/components/TourList/TourList.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react"
 import "./TourList.scss"
 import Tour from "../Tour/Tour"
+import { tourData } from "../tourData"
 
 class TourList extends Component {
+	state = {
+		tours: tourData,
+	}
+
 	render() {
+		// console.log(this.state.tours)
+
+		const { tours } = this.state
+
 		return (
 			<section className="tourlist">
-				<Tour />
+				{/* <Tour  /> */}
+				{tours.map(tour => {
+					return <Tour key={tour.id} tour={tour} />
+				})}
 			</section>
 		)
 	}
