1cc8586234af0d592cc2f4bff65c19e87c37fcb5
Peter <Posoo617@gmail.com>
2020-07-27 13:34:30 +0300

    changes to tour and test deploy

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -17,7 +17,7 @@ class Tour extends Component {
 		return (
 			<article className="tour">
 				<div className="img-container">
-					<img src={img} alt="City Image" />
+					<img src={img} alt="City Depiction Sample" />
 					<span className="close-btn" onClick={() => removeTour(id)}>
 						<i className="fas fa-window-close"></i>
 					</span>
